8034e1d93c54eca6c4ff32d1e9f2631eeb9bd62e
Pablo Arellano <pablo.arellano@unbounce.com>
2021-04-27 17:26:59 -0700

    writing tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,37 @@
-import {fetchRedditApi} from './index';
+import {fetchRedditApi, sanitizeString, formatListings} from './index';
 
 describe('reddit api', () => {
   test('it works', async () => {
-    const listings = await fetchRedditApi();
-    console.log(listings);
+    // const listings = await fetchRedditApi();
+    // console.log(listings);
     // expect(true).toBe(true);
   });
 });
+
+describe('sanitize string', () => {
+  test('filter stars', () => {
+    const result = sanitizeString('whatever* we want');
+    expect(result).toBe('whatever we want');
+  });
+  test('filter underscores', () => {
+    const result = sanitizeString('whatever_ we want');
+    expect(result).toBe('whatever we want');
+  });
+});
+
+describe('format listings', () => {
+  test('formats data with stars', () => {
+    const result = formatListings([
+      {
+        kind: 't1',
+        data: {
+          body: 'GCU *Pure Big Mad Boat Man*',
+          permalink: 'https://example.com',
+        },
+      },
+    ]);
+    expect(result).toEqual(['GCU Pure Big Mad Boat Man']);
+  });
+});
+
+// body: 'GCU *Pure Big Mad Boat Man*'
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import {Listing, Post} from './types';
 const URL = `https://www.reddit.com/r/TheCulture/comments/megvqx/which_culture_ship_name_would_best_replace_the.json`;
 
 export const getBody = (item: Post) => item.data.body;
+export const splitPost = (item: string) => item.split('\n');
+export const getNonEmptyStrings = (item: string) => item !== '';
+export const sanitizeString = (item: string) => item.replace(/_|\*/g, '');
+
 export async function fetchListings(url: string): Promise<Listing[]> {
   const response = await fetch(url);
   return response.json();
@@ -11,14 +15,16 @@ export async function fetchListings(url: string): Promise<Listing[]> {
 
 export async function fetchRedditApi() {
   const listings = await fetchListings(URL);
-  const splitItems = listings[1]?.data.children
-    .map(getBody)
-    .map(item => item.split('\n'))
-    .flat()
-    .filter(item => item !== '')
-    .map(item => item.replace(/_|\*/g, ''));
+  return formatListings(listings[1]?.data.children);
+}
 
-  return splitItems;
+export function formatListings(posts: Post[]) {
+  return posts
+    .map(getBody)
+    .map(splitPost)
+    .flat()
+    .filter(getNonEmptyStrings)
+    .map(sanitizeString);
 }
 
 // main();
