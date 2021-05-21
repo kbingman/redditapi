bbb40be9f0c0a11b1e6c845273469c2bba389195
brunagarcia <garciabrunap@gmail.com>
2021-05-20 18:44:21 -0700

    may 20 sesh - compose and pipe

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,36 +1,35 @@
-import {fetchRedditApi, sanitizeString, formatListings} from './index';
+import { fetchRedditApi, sanitizeString, formatListings } from "./index";
 
-describe('reddit api', () => {
-  test('it works', async () => {
-    // const listings = await fetchRedditApi();
-    // console.log(listings);
-    // expect(true).toBe(true);
+describe("reddit api", () => {
+  test.only("it works", async () => {
+    const listings = await fetchRedditApi();
+    expect(true).toBe(true);
   });
 });
 
-describe('sanitize string', () => {
-  test('filter stars', () => {
-    const result = sanitizeString('whatever* we want');
-    expect(result).toBe('whatever we want');
+describe("sanitize string", () => {
+  test("filter stars", () => {
+    const result = sanitizeString("whatever* we want");
+    expect(result).toBe("whatever we want");
   });
-  test('filter underscores', () => {
-    const result = sanitizeString('whatever_ we want');
-    expect(result).toBe('whatever we want');
+  test("filter underscores", () => {
+    const result = sanitizeString("whatever_ we want");
+    expect(result).toBe("whatever we want");
   });
 });
 
-describe('format listings', () => {
-  test('formats data with stars', () => {
+describe("format listings", () => {
+  test("formats data with stars", () => {
     const result = formatListings([
       {
-        kind: 't1',
+        kind: "t1",
         data: {
-          body: 'GCU *Pure Big Mad Boat Man*',
-          permalink: 'https://example.com',
+          body: "GCU *Pure Big Mad Boat Man*",
+          permalink: "https://example.com",
         },
       },
     ]);
-    expect(result).toEqual(['GCU Pure Big Mad Boat Man']);
+    expect(result).toEqual(["GCU Pure Big Mad Boat Man"]);
   });
 });
 
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import fetch from 'isomorphic-unfetch';
-import {Listing, Post} from './types';
+import fetch from "isomorphic-unfetch";
+import { Listing, Post } from "./types";
 
 const URL = `https://www.reddit.com/r/TheCulture/comments/megvqx/which_culture_ship_name_would_best_replace_the.json`;
 
 export const getBody = (item: Post) => item.data.body;
-export const splitPost = (item: string) => item.split('\n');
-export const getNonEmptyStrings = (item: string) => item !== '';
-export const sanitizeString = (item: string) => item.replace(/_|\*/g, '');
+export const splitPost = (item: string) => item.split("\n");
+export const getNonEmptyStrings = (item: string) => item !== "";
+export const sanitizeString = (item: string) => item.replace(/_|\*/g, "");
 
 export async function fetchListings(url: string): Promise<Listing[]> {
   const response = await fetch(url);
@@ -15,9 +15,17 @@ export async function fetchListings(url: string): Promise<Listing[]> {
 
 export async function fetchRedditApi() {
   const listings = await fetchListings(URL);
-  return formatListings(listings[1]?.data.children);
+  // return formatListings(getChildren(listings));
+  const composeFunction = ourOwnCompose(
+    loggerFunc,
+    formatListings,
+    getChildren,
+    loggerFunc
+  );
+  return composeFunction(listings);
 }
 
+//This is also sort of a pipe
 export function formatListings(posts: Post[]) {
   return posts
     .map(getBody)
@@ -26,5 +34,33 @@ export function formatListings(posts: Post[]) {
     .filter(getNonEmptyStrings)
     .map(sanitizeString);
 }
+
+export const getChildren = (listing: Listing[]) => listing[1]?.data.children;
+
+//This func doesn't mean to do anything it's a test to pass our parameters.
+export const identityFunc = <T>(arg: T): T => arg;
+
+export const loggerFunc = <T>(arg: T): T => {
+  console.log(arg);
+  return arg;
+};
+
+//This is not a compose func because it goes right to the left.
+export const ourOwnCompose =
+  (...fns: Function[]) =>
+  (arg: any) => {
+    return fns.reduceRight((acc, fn) => {
+      return fn(acc);
+    }, arg);
+  };
+
+//This is not a compose func because it goes left to right.
+export const ourOwnPipe =
+  (...fns: any[]) =>
+  (arg: any) => {
+    return fns.reduce((acc, fn) => {
+      return fn(acc);
+    }, arg);
+  };
 
 // main();
