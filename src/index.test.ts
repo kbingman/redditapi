48fd26d03a7451ba1719004ce5f29c027fc9e786
Joslin Chan <joslin.chan@unbounce.com>
2021-07-06 20:07:14 -0700

    Compose & pipe fns with multiple types + compose fn overload

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,45 +1,78 @@
-import { fetchRedditApi, sanitizeString, formatListings, ourOwnPipeWithTwoArgs } from "./index";
+import {
+  fetchRedditApi,
+  sanitizeString,
+  formatListings,
+  ourOwnPipeWithTwoArgs,
+  ourOwnComposeWithTwoArgsDiffTypes,
+  ourOwnComposeOverload,
+} from './index';
 
-describe("reddit api", () => {
-  test.only("it works", async () => {
+describe('reddit api', () => {
+  test('it works', async () => {
     const listings = await fetchRedditApi();
     expect(true).toBe(true);
   });
 });
 
-describe("sanitize string", () => {
-  test("filter stars", () => {
-    const result = sanitizeString("whatever* we want");
-    expect(result).toBe("whatever we want");
+describe('sanitize string', () => {
+  test('filter stars', () => {
+    const result = sanitizeString('whatever* we want');
+    expect(result).toBe('whatever we want');
   });
-  test("filter underscores", () => {
-    const result = sanitizeString("whatever_ we want");
-    expect(result).toBe("whatever we want");
+  test('filter underscores', () => {
+    const result = sanitizeString('whatever_ we want');
+    expect(result).toBe('whatever we want');
   });
 });
 
-describe("format listings", () => {
-  test("formats data with stars", () => {
+describe('format listings', () => {
+  test('formats data with stars', () => {
     const result = formatListings([
       {
-        kind: "t1",
+        kind: 't1',
         data: {
-          body: "GCU *Pure Big Mad Boat Man*",
-          permalink: "https://example.com",
+          body: 'GCU *Pure Big Mad Boat Man*',
+          permalink: 'https://example.com',
         },
       },
     ]);
-    expect(result).toEqual(["GCU Pure Big Mad Boat Man"]);
+    expect(result).toEqual(['GCU Pure Big Mad Boat Man']);
   });
 });
 
 describe('our own pipe with two fns', () => {
   const add1 = (n: number) => n + 1;
   const multiply2 = (n: number) => n * 2;
-  test.only('bla bla', () => {
+  test('bla bla', () => {
     const results = ourOwnPipeWithTwoArgs(add1, multiply2);
     console.log(results(222));
   });
 });
 
 // body: *Pure Big Mad Boat Man*'
+
+describe('our Own Compose With Two Args Diff Types', () => {
+  const add1 = (n: number) => n + 1;
+  const multiply2 = (n: number) => n * 2;
+  const sayHola = (n: number): string => n + 'Hola';
+  const yell = (s: string) => s + '!!!';
+
+  test('compose two fns', () => {
+    const addAndMultiply = ourOwnComposeWithTwoArgsDiffTypes(yell, sayHola);
+
+    console.log(addAndMultiply(42));
+  });
+});
+
+describe('our Own Compose Overload With Multiple Args Diff Types', () => {
+  const add1 = (n: number) => n + 1;
+  const multiply2 = (n: number) => n * 2;
+  const sayHola = (n: number): string => n + 'Hola';
+  const yell = (s: string) => s + '!!!';
+
+  test.only('compose multi fns', () => {
+    const addAndMultiply = ourOwnComposeOverload(yell, sayHola);
+
+    console.log(addAndMultiply(42));
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import fetch from "isomorphic-unfetch";
-import { Listing, Post } from "./types";
+import fetch from 'isomorphic-unfetch';
+import { Listing, Post } from './types';
 
 const URL = `https://www.reddit.com/r/TheCulture/comments/megvqx/which_culture_ship_name_would_best_replace_the.json`;
 
 export const getBody = (item: Post) => item.data.body;
-export const splitPost = (item: string) => item.split("\n");
-export const getNonEmptyStrings = (item: string) => item !== "";
-export const sanitizeString = (item: string) => item.replace(/_|\*/g, "");
+export const splitPost = (item: string) => item.split('\n');
+export const getNonEmptyStrings = (item: string) => item !== '';
+export const sanitizeString = (item: string) => item.replace(/_|\*/g, '');
 
 export async function fetchListings(url: string): Promise<Listing[]> {
   const response = await fetch(url);
@@ -45,7 +45,7 @@ export const loggerFunc = <T>(arg: T): T => {
   return arg;
 };
 
-//This is not a compose func because it goes right to the left.
+//This is a compose func because it goes right to the left.
 export const ourOwnCompose =
   (...fns: Function[]) =>
   (arg: any) => {
@@ -65,12 +65,93 @@ export const ourOwnPipe =
 
 //Typing away with generics
 export const ourOwnPipeWithTwoArgs =
-<T>(...fns:[fn1: (arg: T) => T , fn2: (arg:T) => T]) =>
-(arg: T) => {
-  return fns.reduce((acc, fn) => {
-    return fn(acc);
-  }, arg);
-};
+  <T>(...fns: [fn1: (arg: T) => T, fn2: (arg: T) => T]) =>
+  (arg: T) => {
+    return fns.reduce((acc, fn) => {
+      return fn(acc);
+    }, arg);
+  };
+
+/* COMPOSE */
+// Typing generics with different types
+// T, U, V are usually used as variables for generics
+// The typing follows the order of Input, Midput, Output
+export const composeWithTwoArgsDiffTypes =
+  <Input, Midput, Output>(
+    fn2: (arg: Midput) => Output,
+    fn1: (arg: Input) => Midput
+  ) =>
+  (arg: Input): Output => {
+    return fn2(fn1(arg));
+  };
+
+// this fn is the same as above
+export function ourOwnComposeWithTwoArgsDiffTypes<Input, Midput, Output>(
+  ...fns: [fn2: (arg: Midput) => Output, fn1: (arg: Input) => Midput]
+) {
+  return (arg: Input): Output => {
+    return fns.reduceRight((acc: any, fn) => {
+      return fn(acc);
+    }, arg);
+  };
+}
+
+/* COMPOSE FUNCTION OVERLOAD */
+/* Overloads allows for multiple arguments with multiple typings in a compose or pipe function */
+
+// overload declarations order does not matter
+// overload declarations must be in the same file as the actual function
+// overload declarations must have the same name as the actual function
+// overload declarations must be prepend with `export`
+// overload declarations must use `function` instead of the fat arrow `=>`
+/*
+  We will need one overload declarations for each situation so that Typescript knows what to expect.
+  For example, if we only have an overload declaration for a situation with 3 arguments and
+  not for the situation with 2 arguments, Typescript will complain that it expects 3 args when
+  we only want to put 2 args in.
+*/
+
+// overload declaration for situation 1 with 2 arguments
+export function ourOwnComposeOverload<Input, Midput, Output>(
+  ...fns: [fn2: (arg: Midput) => Output, fn1: (arg: Input) => Midput]
+): (arg: Input) => Output;
+
+// overload declaration for situation 2 with 3 arguments
+export function ourOwnComposeOverload<Input, Midput1, Midput2, Output>(
+  ...fns: [
+    fn3: (arg: Midput2) => Output,
+    fn2: (arg: Midput1) => Midput2,
+    fn1: (arg: Input) => Midput1
+  ]
+): (arg: Input) => Output;
+
+// This is the actual function
+// This is a compose func because it goes right to the left.
+/*
+  If we only write the overload declaration for situation 1 with 2 arguments, this fn is the same as the one above called ourOwnComposeWithTwoArgsDiffTypes
+*/
+// `...fns` is the same as `...args`, the name `fns` is made up
+// `acc` can be `any` type because this allows for the type to change as our function progresses
+// this is the proper way to use the `any` type
+export function ourOwnComposeOverload<Input, Output>(...fns: Function[]) {
+  return (arg: Input): Output => {
+    return fns.reduceRight((acc: any, fn) => {
+      return fn(acc);
+    }, arg);
+  };
+}
+
+// PIPE
+//Typing away with generics with different types
+// T, U, V for generics
+export const ourOwnPipeWithTwoArgsDiffTypes =
+  <Input, Midput, Output>(
+    fn1: (arg: Input) => Midput,
+    fn2: (arg: Midput) => Output
+  ) =>
+  (arg: Input): Output => {
+    return fn2(fn1(arg));
+  };
 
 //Attempt with different parameters
 // export const ourOwnPipeWithTwoArgs =
@@ -82,13 +163,13 @@ export const ourOwnPipeWithTwoArgs =
 // };
 
 const toUpperCase = (s: string) => s.toUpperCase();
-const yell = (s: string) => s+'!!!'
+const yell = (s: string) => s + '!!!';
 const add1 = (n: number) => n + 1;
 const multiply2 = (n: number) => n * 2;
 
-//Wont work with same type for args and return value. 
+//Wont work with same type for args and return value.
 const toString = (n: number) => n.toString();
 
-console.log(ourOwnPipeWithTwoArgs(toUpperCase, yell)('hola'))
+console.log(ourOwnPipeWithTwoArgs(toUpperCase, yell)('hola'));
 
 // main();
